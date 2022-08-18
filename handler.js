8b47701c8168ae4a827fad5c8cbafe1c5171ccc6
Ponja94 <henrique.danielb@gmail.com>
2022-08-18 16:27:24 -0300

    12th commit

---
+++ handler.js
@@ -0,0 +1,118 @@
+
+$(document).ready(function(){
+    $(document).on('click', '.delete', function(){
+
+        var id = $(this).data('id');
+        //console.log("id é :" +id);
+        $.ajax({
+            url:"script.php",
+            method:"POST",
+            data:{action:'delete', id:id},
+            dataType:"text",
+            success:function(data)
+            {
+                window.location.reload();
+                console.log(data);
+            }
+        });
+        
+
+    });
+
+    $(document).on('click', '.del-filho', function(){
+
+        var id_filho = $(this).data('id');
+        var id_pai = $(this).data('pai');
+
+        console.log("id pai é: "+id_pai+" e id filho é :" +id_filho);
+
+        $.ajax({
+            url:"script.php",
+            method:"POST",
+            data:{action:'del-filho', id_pai:id_pai,id_filho:id_filho},
+            dataType:"text",
+            success:function(data){
+
+                console.log(data);
+                window.location.reload();
+            }
+        });
+        
+
+    });
+
+    $(document).on('click', '.novo-filho', function(){
+
+        var id_pai = $(this).data('pai');
+        var nome = prompt("Informe o nome:");
+        console.log("id pai é: "+id_pai+" nome: " +nome);
+        if(nome != null){
+            $.ajax({
+                url:"script.php",
+                method:"POST",
+                data:{action:'novo-filho', id_pai:id_pai, nome:nome},
+                dataType:"text",
+                success:function(data){
+
+                    console.log(data);
+                    window.location.reload();
+                }
+            });
+        }
+
+    });
+
+    $(document).on('click', '.gravar-bd', function(){
+        
+        $.ajax({
+            url:"script.php",
+            method:"POST",
+            data:{action:'gravar-bd'}, 
+            dataType:"text",
+            success:function(data){
+                //console.log(data);
+                window.location.reload();
+                alert("Gravado com sucesso!");
+            }
+        })
+
+    });
+
+    $(document).on('click', '.ler-bd', function(){
+
+        console.log('top aqui1');
+        
+        $.ajax({
+            url:"script.php",
+            method:"POST",
+            data:{action:'ler-bd'}, 
+            dataType:"text",
+            success:function(data){
+                console.log(data);
+                window.location.reload();
+            }
+        })
+
+    });
+
+    $(document).on('click', '.pessoa-adicionada', function(){ 
+
+        var input = document.querySelector("#entrada").value;
+
+
+        if(input != null && input !=""){
+            $.ajax({
+                url:"script.php",
+                method:"POST",
+                data:{action:'pessoa-adicionada', input:input}, 
+                dataType:"text",
+                success:function(data){
+                    //console.log(data);
+                    window.location.reload();
+                }
+            })
+        }
+
+    });
+
+});
